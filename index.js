2e08e4c189a0af81a87cad7a8724bf78c4d53fde
kasididd <kasidid.wan@gmail.com>
2022-10-19 16:56:09 +0700

    parser

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = 4000
+var HTMLParser = require('node-html-parser');
 
+var root = HTMLParser.parse('<ul id="list"><li>Hello World</li></ul>');
 app.listen(PORT,()=>{
     console.log('http://localhost:'+PORT);
 })
 
 app.get('/',(req,res)=>{
-    res.send('open'+"<br/>"+
-    "<button> click to create files </button>")
+    res.send(root)
 })
 
 module.exports = app
