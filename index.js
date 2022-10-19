c99bcdcab5136761e48b97b414ceb2f7919eadd5
kasididd <kasidid.wan@gmail.com>
2022-10-19 16:43:27 +0700

    new file

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express();
 const PORT = 4000
 
 app.listen(PORT,()=>{
-    console.log('listening on port '+PORT);
+    console.log('http://localhost:'+PORT);
 })
 
 app.get('/',(req,res)=>{
-    res.send('this is my APU running....')
+    res.send('this is my APU running....'+"<br/>"+
+    "<button> click to create files </button>")
 })
 
 module.exports = app
