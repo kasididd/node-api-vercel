bd869c666c4b10c2919792d8a13a68ed6f8bbbe0
kasididd <kasidid.wan@gmail.com>
2022-10-19 17:01:34 +0700

    fs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 4000
+const fs = require('fs');
 var HTMLParser = require('node-html-parser');
 
 var root = HTMLParser.parse('<ul id="list"><li>Hello World</li></ul>');
@@ -11,5 +12,16 @@ app.listen(PORT,()=>{
 app.get('/',(req,res)=>{
     res.send(root)
 })
-
+  
+let data = "This is a file containing a collection of books.";
+  
+fs.writeFile("books.txt", data, (err) => {
+  if (err)
+    console.log(err);
+  else {
+    console.log("File written successfully\n");
+    console.log("The written has the following contents:");
+    console.log(fs.readFileSync("books.txt", "utf8"));
+  }
+});
 module.exports = app
